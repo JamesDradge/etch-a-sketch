b6c02c297754fbd0652db8c6e7318639668991d0
JamesDradge <bobdradge@hotmail.com>
2021-12-07 23:03:29 +0000

    Added reset button

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ let numberOfDivs = gridRows * gridRows;
 const gridPixels = 400;
 const gridSizePixels = gridPixels/gridRows - 2;
 const container = document.querySelector(".grid-container");
-
-function createGridDivs() {
-    let gridItem = document.createElement('div');
-    gridItem.className = 'grid-item';
-    gridItem.setAttribute('style', `width: ${gridSizePixels}px; height: ${gridSizePixels}px;`)
-    container.appendChild(gridItem);
-}
+const resetBtn = document.querySelector(".btn");
 
 for (let i=0; i<numberOfDivs; i++) {
     createGridDivs();
@@ -23,8 +17,17 @@ gridItems.forEach((div) => {
     });
 });
 
-gridItems.forEach((div) => {
-    div.addEventListener('touchmove', (e) => {
-        div.classList.add('black');
-    });
-});
+resetBtn.addEventListener("click", resetGrid);
+
+function createGridDivs() {
+    let gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+    gridItem.setAttribute('style', `width: ${gridSizePixels}px; height: ${gridSizePixels}px;`)
+    container.appendChild(gridItem);
+}
+
+function resetGrid() {
+    gridItems.forEach((div) => {
+        div.classList = 'grid-item';
+    })
+}
