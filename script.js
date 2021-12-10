b5d4de00f9f656de7aaafb7d79c531b0a0523f75
JamesDradge <bobdradge@hotmail.com>
2021-12-10 19:50:02 +0000

    Added in functions to support re-sizing the grid based on player input

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 let gridRows = 16;
 let numberOfDivs = gridRows * gridRows;
-const gridPixels = 400;
-const gridSizePixels = gridPixels/gridRows;
+let gridPixels = 400;
+let gridSizePixels = gridPixels/gridRows;
 const container = document.querySelector(".grid-container");
+const resizeBtn = document.querySelector("#resize-btn");
 const resetBtn = document.querySelector(".btn");
 
-for (let i=0; i<numberOfDivs; i++) {
-    createGridDivs();
+window.onload = createGrid();
+resetBtn.addEventListener("click", resetGrid);
+resizeBtn.addEventListener("click", resizeGrid);
+
+function createGrid() {
+    for (let i=0; i<numberOfDivs; i++) {
+        createGridDivs();
+    }
+
+    gridItems = document.querySelectorAll('.grid-item');
+
+    gridItems.forEach((div) => {
+        div.addEventListener('mouseenter', (e) => {
+            div.classList.add('black');
+        });
+    });
 }
 
-gridItems = document.querySelectorAll('.grid-item');
-
-gridItems.forEach((div) => {
-    div.addEventListener('mouseenter', (e) => {
-        div.classList.add('black');
-    });
-});
-
-resetBtn.addEventListener("click", resetGrid);
+function deleteGrid() {
+    gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((div) => {
+        div.remove();
+    })
+}
 
 function createGridDivs() {
     let gridItem = document.createElement('div');
@@ -30,4 +42,21 @@ function resetGrid() {
     gridItems.forEach((div) => {
         div.classList = 'grid-item';
     })
+}
+
+function calculateSizing() {
+    numberOfDivs = gridRows * gridRows;
+    gridSizePixels = gridPixels/gridRows;
+}
+
+function resizeGrid() {
+    let input = prompt("Please enter the number of rows you would like the grid to be");
+    if (isNaN(input) || input < 1 || input > 100) {
+        alert("Please enter a number between 1 - 100.");
+    } else {
+        gridRows = input;
+        deleteGrid();
+        calculateSizing();
+        createGrid();
+    }
 }
